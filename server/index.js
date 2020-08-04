1bcbeb27d4781609dd771bf81aa02c11f799e02a
DestinTech <destintech@gmail.com>
2020-08-04 04:10:29 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Allow dotfiles - this is required for verification by Lets Encrypt's certbot
+app.use(express.static(path.join(__dirname, 'build'), {dotfiles: 'allow'}));
+
+
+app.get('/', (req,res) => {
+	res.send("hello world!");
+});
+
+app.listen(3001);
