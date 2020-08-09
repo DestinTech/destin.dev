920b0d81d84fcea70289ee494feefcdd4f1f1911
DestinTech <destintech@gmail.com>
2020-08-09 02:29:57 -0600

    set up a basic web server with express to allow access to the public directory inside the server folder. Added a 404 page and set up middleware to direct to the file when 404'd

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
+var http = require('http');
 const path = require('path');
+const fs = require('fs');
+
+
 const app = express();
 
 // Allow dotfiles - this is required for verification by Lets Encrypt's certbot
 app.use(express.static(path.join(__dirname, 'build'), {dotfiles: 'allow'}));
 
+// include the public directory, use __dirname to prevent cross system conpatability issues
 
-app.get('/', (req,res) => {
-	res.send("test");
+
+app.use(express.static(path.join(__dirname ,"/public")));
+
+app.use(function(req,res,next){
+	res.status(404).sendFile(path.join(__dirname + "/public/404.html"));
 });
 
-app.listen(3001);
+app.listen(3004);
